e3353b5e7b7b995dbf62583dbe91037c8b9655af
Ever Garcia <uriel621@live.com>
2016-12-21 01:07:13 -0800

    Firebase added

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var config = {
+    apiKey: "AIzaSyBk6WXby6c3gI86VyPu6unMM0oDAF_3wP8",
+    authDomain: "contacts-eef38.firebaseapp.com",
+    databaseURL: "https://contacts-eef38.firebaseio.com",
+    storageBucket: "contacts-eef38.appspot.com",
+    messagingSenderId: "994995999555"
+};
+firebase.initializeApp(config);
