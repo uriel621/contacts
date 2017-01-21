f481a10dde947588256cb3a639604c6dc5a3f7f7
Ever Garcia <uriel621@live.com>
2017-01-21 01:14:06 -0800

    connect server to html

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 var express = require("express");
 var app = express();
 
-app.get("/", function(req, res){
-    res.send("Hello world from server.js")
-});
+//For The HTML Template
+app.use(express.static(__dirname + "/public"));
 
 app.listen(3000);
     console.log("Server running on port 3000")
