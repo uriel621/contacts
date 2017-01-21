0d1e2a5c15d906045c1f6b67058c9aaa6d3391e3
Ever Garcia <uriel621@live.com>
2017-01-21 00:58:57 -0800

    node and express

--- js/script.js
+++
@@ -1,8 +0,0 @@
-var config = {
-    apiKey: "AIzaSyBk6WXby6c3gI86VyPu6unMM0oDAF_3wP8",
-    authDomain: "contacts-eef38.firebaseapp.com",
-    databaseURL: "https://contacts-eef38.firebaseio.com",
-    storageBucket: "contacts-eef38.appspot.com",
-    messagingSenderId: "994995999555"
-};
-firebase.initializeApp(config);
