e3cde64606ac6db7587f1ff8d44c37edc1d85505
Ever Garcia <uriel621@live.com>
2017-01-21 03:41:02 -0800

    get data from server

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -2,4 +2,10 @@ var ever = angular.module("contactApp", []);
 
 ever.controller("AppCtrl", ["$scope", "$http", function ($scope, $http) {
     console.log("another test")
+
+    $http.get('/contactList').then(function(response) {
+    console.log('I got the requested data');
+    $scope.contactList = response.data;
+  });
+
 }])
--- server.js
+++ server.js
@@ -4,5 +4,28 @@ var app = express();
 //For The HTML Template
 app.use(express.static(__dirname + "/public"));
 
+app.get("/contactList", function(req, res){
+    console.log("I received a GET request")
+    
+    person1 = {
+        name: "Tim",
+        email: "tim@gmail.com",
+        number: 5555555555
+    }
+    person2 = {
+        name: "Tqqqqqqqqqqim",
+        email: "tim@gmqqqqqqqqqqail.com",
+        number: 55555555555555555555555511111111111115555
+    }
+    person3 = {
+        name: "Ticcm",
+        email: "tim@gmccail.com",
+        number: 5555555555
+    }
+
+    var contactList = [person1, person2, person3]
+    res.json(contactList)
+})
+
 app.listen(3000);
     console.log("Server running on port 3000")
