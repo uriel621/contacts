cf04b0698e88f07aa130e8b604680b2024d2498e
Ever Garcia <uriel621@live.com>
2017-01-21 16:51:42 -0800

    add to database

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -3,9 +3,32 @@ var ever = angular.module("contactApp", []);
 ever.controller("AppCtrl", ["$scope", "$http", function ($scope, $http) {
     console.log("another test")
 
-    $http.get('/contactList').then(function(response) {
-    console.log('I got the requested data');
-    $scope.contactList = response.data;
-  });
+    var refresh = function () {
+        $http.get('/contactList').then(function (response) {
+            console.log('I got the requested data');
+            $scope.contactList = response.data;
+            $scope.contact = null
+        });
+    }
+    
+    refresh()
+
+    $scope.addContact = function () {
+        console.log($scope.contact)
+
+        $http.post("/contactList", $scope.contact).then(function (response) {
+            console.log(response)
+            refresh()
+        })
+    }
+
+    $scope.deleteContact = function (id) {
+        console.log(id)
+        $http.delete("/contactList/:id" + id).then(function(response){
+            console.log(response)
+            refresh()
+        })
+        
+    }
 
 }])
--- server.js
+++ server.js
@@ -1,31 +1,37 @@
 var express = require("express");
 var app = express();
+var mongojs = require("mongojs");
+var db = mongojs("contactList", ["contactList"]);
+var bodyParser = require("body-parser");
 
 //For The HTML Template
 app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.json())
 
 app.get("/contactList", function(req, res){
     console.log("I received a GET request")
     
-    person1 = {
-        name: "Tim",
-        email: "tim@gmail.com",
-        number: 5555555555
-    }
-    person2 = {
-        name: "Tqqqqqqqqqqim",
-        email: "tim@gmqqqqqqqqqqail.com",
-        number: 55555555555555555555555511111111111115555
-    }
-    person3 = {
-        name: "Ticcm",
-        email: "tim@gmccail.com",
-        number: 5555555555
-    }
+    db.contactList.find(function(err, docs){
+        console.log(docs);
+        res.json(docs)
+    })
+})
 
-    var contactList = [person1, person2, person3]
-    res.json(contactList)
+//listens for post reuest from the controller
+app.post("/contactList", function(req, res){
+    console.log(req.body)
+    db.contactList.insert(req.body, function(err, doc){
+        res.json(doc)
+    })
+})
+
+app.delete("/contactList/:id", function(req, res){
+    var id = req.params.id;
+    console.log(id)
+    db.contactList.remove({_id: mongojs.ObjectId(id)}, function(err, doc){
+            res.json(doc);
+        })
 })
 
 app.listen(3000);
-    console.log("Server running on port 3000")
+console.log("Server running on port 3000")
